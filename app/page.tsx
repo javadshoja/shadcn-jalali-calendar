41736a9a11270e2e12fe9f750670952063b972ef
Javad Shoja <ssjavad00@gmail.com>
2025-05-29 12:22:47 +0330

    Fix registry issues for open in v0 button (#4)
    * refactor(main): update main box width
    
    * refactor code style
    
    * refactor(Home): adjust layout and styling for improved responsiveness
    
    - Remove unnecessary overflow-y-scroll class from the main container.
    - Ensure footer has a consistent height for better alignment.
    
    * fix(OpenInV0Button): update URL format for v0 endpoint
    
    * feat(JalaliCalendar): add new page component for Jalali calendar integration
    
    * feat(JalaliComponents): add new Jalali calendar and date picker components with dependencies
    
    * feat(JalaliComponents): introduce Jalali calendar and date picker components with popover support

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { cn } from '@/lib/utils'
 
 export default function Home() {
 	return (
-		<div className='relative lg:h-svh flex flex-col gap-y-14 overflow-y-scroll'>
-			<main className='relative mx-auto flex max-w-[90vw] items-center justify-center overflow-hidden sm:max-w-screen-lg sm:p-10'>
+		<div className='relative lg:h-svh flex flex-col gap-y-14'>
+			<main className='relative mx-auto flex max-w-[90vw] items-center justify-center overflow-hidden sm:max-w-screen-lg sm:p-10 lg:h-full'>
 				<GridPattern
 					squares={[
 						[4, 4],
@@ -27,7 +27,7 @@ export default function Home() {
 					)}
 				/>
 				<ThemeMenuButton className='fixed right-[5vw] top-5 flex-1 md:right-5' />
-				<div className='flex w-full flex-col items-center rounded-lg border px-5 mt-16 backdrop-blur-md supports-[backdrop-filter]:bg-background/40 py-2 sm:py-4'>
+				<div className='flex w-full flex-col items-center rounded-lg border px-5 mt-16 backdrop-blur-md supports-[backdrop-filter]:bg-background/40 py-2 sm:py-4 lg:min-w-[944px]'>
 					<div className='grid lg:grid-cols-2 gap-4 w-full'>
 						<div className='flex items-start flex-col py-2 w-full'>
 							<h1 className='mb-3 w-full text-3xl font-bold text-center sm:w-96'>
@@ -52,7 +52,7 @@ export default function Home() {
 				</div>
 			</main>
 
-			<footer className='flex w-full mb-0 mt-auto pb-4 px-3'>
+			<footer className='flex w-full mb-0 mt-auto pb-4 px-3 h-9'>
 				<p className='lg:text-center text-sm text-muted-foreground mx-auto'>
 					ساخته شده با ❤️ توسط{' '}
 					<a
--- registry/new-york/jalali-calendar/page.tsx
+++ registry/new-york/jalali-calendar/page.tsx
@@ -1,10 +1,10 @@
-import { JalaliDatePicker } from '@/registry/new-york/jalali-date-picker/jalali-date-picker'
+import { JalaliCalendar } from './jalali-calendar'
 
 export default function Page() {
 	return (
 		<div className='flex min-h-svh w-full items-center justify-center p-6 md:p-10'>
 			<div className='w-full max-w-sm'>
-				<JalaliDatePicker />
+				<JalaliCalendar />
 			</div>
 		</div>
 	)
